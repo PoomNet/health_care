d76913d2eefe6a8d52bf875d7f615b37e337ec0a
xzsawq47 <60070074@kmitl.ac.th>
2019-05-22 18:14:41 +0700

    add filter

--- lib/screens/post_screens.dart
+++ lib/screens/post_screens.dart
@@ -12,88 +12,274 @@ class PostPage extends StatefulWidget {
 }
 
 class _PostPageState extends State<PostPage> {
+  var category = 'all';
+  int _radioValue = 7;
+  var filter = false;
+
   var comment = [];
   var post = [];
   var user;
   var check_user = 0;
   var check_post = 0;
-  var new_com = 0;
 
-  Widget _buildPost(BuildContext context, DocumentSnapshot document) {
-    var check_user = 0;
-  var check_post = 0;
-    return Card(
-        child: ListTile(
-      title: Text(document['cause']),
-      subtitle: Text(document['symptom']),
-      onTap: () {
-        Currentpost.CAUSE = document['cause'];
-        Currentpost.SYMPTOM = document['symptom'];
-        Currentpost.DESCRIBE = document['describe'];
-        Currentpost.CATEGORY = document['category'];
-        Currentpost.USER = document['user'];
-        Currentpost.IMAGE = document['image'];
-        FirebaseDatabase.instance.reference().once().then((DataSnapshot data) {
-      for (check_user; check_user < data.value.length; check_user++) {
-        if (data.value[check_user] != null) {
-          if (data.value[check_user]['user']['name'] == Currentpost.USER) {
-            user = data.value[check_user]['user'];
-            break;
-          }
-        }
-      }
-      if (data.value[check_user]['post'] != null) {
-        for (check_post;
-            check_post < data.value[check_user]['post'].length;
-            check_post++) {
-              print(123123123);
-              print(data.value[check_user]['post'][check_post]['describe']);
-              print(Currentpost.DESCRIBE);
-          if (data.value[check_user]['post'][check_post]['describe'] ==
-              Currentpost.DESCRIBE) {
-            break;
-          }
-        }
-      }
-      comment = data.value[check_user]['post'][check_post]['comment'];
-      new_com = data.value[check_user]['post'][check_post]['comment'].length;
-      Currentpost.COMMENT= comment;
-      print(Currentpost.COMMENT);
-      print(comment);
+  void _handleRadioValueChange(int value) {
+    setState(() {
+      _radioValue = value;
     });
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => Showpost()));
-      },
-    ));
+    setState(() {
+      switch (_radioValue) {
+        case 7:
+          category = 'all';
+          break;
+        case 0:
+          category = 'การกิน';
+          break;
+        case 1:
+          category = 'การออกกำลังกาย';
+          break;
+        case 2:
+          category = 'ลดน้ำหนัก';
+          break;
+        case 3:
+          category = 'อุบัติเหตุ';
+          break;
+        case 4:
+          category = 'ผู้หญิง';
+          break;
+        case 5:
+          category = 'ผู้ชาย';
+          break;
+        case 6:
+          category = 'อื่นๆ';
+          break;
+      }
+    });
   }
 
+  Widget _buildfilterBut() {
+    if (!filter) {
+      return RaisedButton(
+        child: Text("Filter"),
+        onPressed: () {
+          setState(() {
+            filter = true;
+          });
+        },
+      );
+    } else {
+      return Center(
+        child: Text("No Post Found"),
+      );
+    }
+  }
+
+  Widget _buildRadio() {
+    if (filter) {
+      return Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              new Radio(
+                value: 7,
+                groupValue: _radioValue,
+                onChanged: _handleRadioValueChange,
+              ),
+              new Text('all'),
+              new Radio(
+                value: 0,
+                groupValue: _radioValue,
+                onChanged: _handleRadioValueChange,
+              ),
+              new Text('การกิน'),
+              new Radio(
+                value: 1,
+                groupValue: _radioValue,
+                onChanged: _handleRadioValueChange,
+              ),
+              new Text('การออกกำลังกาย'),
+              new Radio(
+                value: 2,
+                groupValue: _radioValue,
+                onChanged: _handleRadioValueChange,
+              ),
+              new Text('ลดน้ำหนัก'),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              new Radio(
+                value: 3,
+                groupValue: _radioValue,
+                onChanged: _handleRadioValueChange,
+              ),
+              new Text('อุบัติเหตุ'),
+              new Radio(
+                value: 4,
+                groupValue: _radioValue,
+                onChanged: _handleRadioValueChange,
+              ),
+              new Text('ผู้หญิง'),
+              new Radio(
+                value: 5,
+                groupValue: _radioValue,
+                onChanged: _handleRadioValueChange,
+              ),
+              new Text('ผู้ชาย'),
+              new Radio(
+                value: 6,
+                groupValue: _radioValue,
+                onChanged: _handleRadioValueChange,
+              ),
+              new Text('อื่นๆ'),
+            ],
+          ),
+          RaisedButton(
+            child: Text("Close"),
+            onPressed: () {
+              setState(() {
+                filter = false;
+              });
+            },
+          )
+        ],
+      );
+    } else {
+      return Column();
+    }
+  }
+
+  Widget _buildPost(BuildContext context, DocumentSnapshot document) {
+    if (category == "all") {
+      var check_user = 0;
+      var check_post = 0;
+      return Card(
+          child: ListTile(
+        title: Text(document['cause']),
+        subtitle: Text(document['symptom']),
+        onTap: () {
+          Currentpost.CAUSE = document['cause'];
+          Currentpost.SYMPTOM = document['symptom'];
+          Currentpost.DESCRIBE = document['describe'];
+          Currentpost.CATEGORY = document['category'];
+          Currentpost.USER = document['user'];
+          Currentpost.IMAGE = document['image'];
+          FirebaseDatabase.instance
+              .reference()
+              .once()
+              .then((DataSnapshot data) {
+            for (check_user; check_user < data.value.length; check_user++) {
+              if (data.value[check_user] != null) {
+                if (data.value[check_user]['user']['name'] ==
+                    Currentpost.USER) {
+                  user = data.value[check_user]['user'];
+                  break;
+                }
+              }
+            }
+            if (data.value[check_user]['post'] != null) {
+              for (check_post;
+                  check_post < data.value[check_user]['post'].length;
+                  check_post++) {
+                if (data.value[check_user]['post'][check_post]['describe'] ==
+                    Currentpost.DESCRIBE) {
+                  break;
+                }
+              }
+            }
+            comment = data.value[check_user]['post'][check_post]['comment'];
+            Currentpost.COMMENT = comment;
+          });
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Showpost()));
+        },
+      ));
+    } else if (category == document['category']) {
+      var check_user = 0;
+      var check_post = 0;
+      print(category);
+      print(document['category']);
+      return Card(
+          child: ListTile(
+        title: Text(document['cause']),
+        subtitle: Text(document['symptom']),
+        onTap: () {
+          Currentpost.CAUSE = document['cause'];
+          Currentpost.SYMPTOM = document['symptom'];
+          Currentpost.DESCRIBE = document['describe'];
+          Currentpost.CATEGORY = document['category'];
+          Currentpost.USER = document['user'];
+          Currentpost.IMAGE = document['image'];
+          FirebaseDatabase.instance
+              .reference()
+              .once()
+              .then((DataSnapshot data) {
+            for (check_user; check_user < data.value.length; check_user++) {
+              if (data.value[check_user] != null) {
+                if (data.value[check_user]['user']['name'] ==
+                    Currentpost.USER) {
+                  user = data.value[check_user]['user'];
+                  break;
+                }
+              }
+            }
+            if (data.value[check_user]['post'] != null) {
+              for (check_post;
+                  check_post < data.value[check_user]['post'].length;
+                  check_post++) {
+                if (data.value[check_user]['post'][check_post]['describe'] ==
+                    Currentpost.DESCRIBE) {
+                  break;
+                }
+              }
+            }
+            comment = data.value[check_user]['post'][check_post]['comment'];
+            Currentpost.COMMENT = comment;
+          });
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Showpost()));
+        },
+      ));
+    }
+    else{
+      return Column();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder(
-        stream: Firestore.instance.collection('post').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    "No Data Found..",
-                    textAlign: TextAlign.center,
-                  )
-                ],
-              ),
-            );
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data.documents.length,
-              itemBuilder: (context, index) =>
-                  _buildPost(context, snapshot.data.documents[index]),
-            );
-          }
-        },
-      ),
-    );
+        body: Column(
+      children: <Widget>[
+        _buildfilterBut(),
+        _buildRadio(),
+        StreamBuilder(
+          stream: Firestore.instance.collection('post').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "No Data Found..",
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              );
+            } else {
+              return Container(
+                  height: 300,
+                  child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index) =>
+                        _buildPost(context, snapshot.data.documents[index]),
+                  ));
+            }
+          },
+        ),
+      ],
+    ));
   }
 }
