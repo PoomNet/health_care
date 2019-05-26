cb552734eb101e03b0ed4d714f8b6160d8fceb8d
Tanakorn <tanakorn.panthaikul@gmail.com>
2019-05-26 23:23:25 +0700

    Update profile and post_desc2

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -16,66 +16,120 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Center(child: Column(
-         children: <Widget>[
-
-            ListTile(
-               leading: Icon(Icons.person),
-               title: Row(children: <Widget>[
-                      Text('User : ',style: TextStyle(fontWeight: FontWeight.bold)),
-                Text(Currentlogin.USER),
-               ],)
-
-           ),
-
-           ListTile(
-               leading: Icon(Icons.email),
-               title: Row(children: <Widget>[
-                  Text('E-mail : ',style: TextStyle(fontWeight: FontWeight.bold)),
-                  Text(Currentlogin.EMAIL),
-               ],)
-
-           ),
-
-          
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RaisedButton(child: Text("YOUR POST"),
-           onPressed: (){
-            //  UserPostPage
-            Navigator.push(context, MaterialPageRoute(builder: (context) => UserPostPage()));
-           },),
-                ),
-           Padding(
-             padding: const EdgeInsets.all(8.0),
-             child: Row(
-              children: <Widget>[
-                 RaisedButton(child: Row(
-                   children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(right : 8.0),
-                        child: Text("THE POST YOU LIKE"),
-                      ),
-                      Icon(Icons.favorite_border)
-                   ],
-                 ),
-             onPressed: (){
-               Navigator.push(context, MaterialPageRoute(builder: (context) => LikePostPage()));
-             },),
-              ],
-             ),
-            
-           )
-              
-            ],
+       body: Container(
+           decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage("assets/pic/bg.jpg"),
+            fit: BoxFit.cover,
           ),
+        ),
+         child: Center(
            
-         
-         ],
-       )),
+           child: Column(
+           children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset('assets/pic/user.png', width: 100,),
+              ),
+              
+              ListTile(
+                 leading: Icon(Icons.person),
+                 title: Center(
+                   child: Row(children: <Widget>[
+                          Text('User : ',style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(Currentlogin.USER),
+                   ],),
+                 )
+
+             ),
+
+             ListTile(
+                 leading: Icon(Icons.email),
+                 title: Row(children: <Widget>[
+                    Text('E-mail : ',style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(Currentlogin.EMAIL),
+                 ],)
+
+             ),
+
+            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              
+              children: <Widget>[
+                RaisedButton(
+              shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+
+
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => UserPostPage()));
+
+              },
+              textColor: Colors.white,
+            padding: const EdgeInsets.all(0.0),
+            child: Container(
+              decoration: new BoxDecoration(
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Color(0xFFF06292),
+                    Color(0xFFF48FB1),
+                    Color(0xFFF8BBD0),
+
+                  ],
+                ),
+                borderRadius: new BorderRadius.circular(20.0),
+
+              ),
+              padding: const EdgeInsets.all(10.0),
+              child: const Text(
+                'Your Post',
+                style: TextStyle(fontSize: 15)
+              ),
+              
+            ),
+          ),
+
+          SizedBox(width: 10,),
+                       RaisedButton(
+              shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+
+
+              onPressed: () {
+                 Navigator.push(context, MaterialPageRoute(builder: (context) => LikePostPage()));
+
+              },
+              textColor: Colors.white,
+            padding: const EdgeInsets.all(0.0),
+            child: Container(
+              decoration: new BoxDecoration(
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Color(0xFFF06292),
+                    Color(0xFFF48FB1),
+                    Color(0xFFF8BBD0),
+
+                  ],
+                ),
+                borderRadius: new BorderRadius.circular(20.0),
+
+              ),
+              padding: const EdgeInsets.all(10.0),
+              child: const Text(
+                'The Post You Like',
+                style: TextStyle(fontSize: 15)
+              ),
+              
+            ),
+          ), 
+     
+                
+              ],
+            ),
+             
+           
+           ],
+         )),
+       ),
     );
   }
 }
